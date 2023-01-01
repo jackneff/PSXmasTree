de3a37e0af9781f5ff29dc707b1af2d13771f646
jack <jack.neff.jr@gmail.com>
2022-12-31 22:30:23 -0800

    update

--- PSXmasTree.ps1
+++ PSXmasTree.ps1
@@ -1,29 +1,39 @@
-function XmasTree {
+# Prints a christmas tree to terminal
+# Variable $Size = (tree_height,trunk_width_trunk_height)
+# Variable $XPos = The starting x position on the terminal line
+# Variables $Colors = List of colors to use
+# Random color pattern each execution
+
+function PSXmasTree {
 
     param (
-        $H=15, 
-        $O=50, 
-        $C = @("blue", "green", "white", "red", "yellow", "magenta"),
-        $X = (Get-Random -Min 0 -Max 5),
-        $TH = 2
+        [array]$Size=@(10,2,2),
+        [int]$XPos=50, 
+        [array]$Colors = @("blue", "green", "white", "red", "yellow", "magenta"),
+        [int]$Idx = (Get-Random -Min 0 -Max ($Colors.Length-1))
     )
    
-    for ( $I=1; $I -le $H; $I++) {
-        $S = " " * ($O - $I) + "*" * ($I * 2) 
-        $X = $N % $C.Length
-        Write-Host $S -ForegroundColor $C[$X]
-        $N++
-    }
+    for ( $i=1; $i -le $Size[0]; $i++) {
+        $Line = " " * ($XPos - $i) + "*" * ($i * 2)
+        $Idx = $Idx % $Colors.Length
+        Write-Host $Line -ForegroundColor $Colors[$Idx]
+        $Idx++
+    }  
 
-    $TW =[math]::floor($I / 4)
-    if ( ($TW % 2) -ne 0 ) { $TW-- }
+    $j = $Size[2]
 
-    while ( $TH -gt 0 ) {
-        $S = " " * ( $O - ( $TW / 2 ) ) + "#" * $TW
-        Write-Host $S -ForegroundColor Yellow
-        $TH--
+    while ( $j -gt 0 ) {
+        $Line = " " * ( $XPos - ( $Size[1] / 2 ) ) + "#" * $Size[1]
+        Write-Host $Line -ForegroundColor DarkRed
+        $j--
     }
 
 }
 
-XmasTree
+PSXmasTree    #default
+
+#PSXmasTree -Size 20,8,4    #big boy
+
+#PSXmasTree -Size 3,2,1 -XPos 100   #totes adorbs baby tree
+
+
